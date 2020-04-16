7be17612790caf596027f429d579ea78351ad6d3
Paul <isakura313@gmail.com>
2020-04-16 20:49:13 +0300

    almost finished

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require("express")
+const favicon = require("express-favicon")
 
 
 port = process.env.PORT || 8080;
 
 const app = express()
 
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+app.use(favicon(__dirname + '/favicon.png'))
+
 app.get('/', (req, res)=>{
-	res.send("Я снова живу")
+	res.render("index")
 })
 
 
 server = app.listen(port, () =>{
 	console.log("сервер запустился")
 	console.log("http://localhost:8080")
+})
+
+const io = require("socket.io")(server)
+
+io.on("connection", (socket)=>{
+	console.log("У нас новый пользователь")
+	socket.username = "Anonim";
+
+	socket.on("change_username", (data)=>{
+		socket.username = data.username;
+		console.log(`Имя было изменено на ${socket.username}`)
+	})
+
+	socket.on("new_message", (data)=>{
+		io.sockets.emit("add_mes", {message:data.message, username: socket.username})
+		console.log(`Всем отправлено сообщение ${data.message}`);
+	})
 })
--- public/code.js
+++ public/code.js
@@ -1,53 +1,52 @@
-const socket = io.connect('http://localhost:8080')
+const socket = io.connect("http://localhost:8080");
 
-let message = document.querySelector("#message")
+let message = document.querySelector("#message");
 
-let send_message = document.querySelector("#send_message")
+let send_message = document.querySelector("#send_message");
 
-let send_username = document.querySelector("#send_username")
+let send_username = document.querySelector("#send_username");
 
-let feedback= document.querySelector("#feedback")
+let feedback = document.querySelector("#feedback");
 
-let show_name= document.querySelector("#show_name")
+let show_name = document.querySelector("#show_name");
 
-
-
-
-function display_name(name){
-	show_name.innerHTML = `Ваш ник <span class="has-text-danger">${name}</span>`;
+function display_name(name) {
+  show_name.innerHTML = `Ваш ник <span class="has-text-danger">${name}</span>`;
 }
+
+send_username.onclick = () => {
+  socket.emit("change_username", { username: username.value });
+  display_name(username.value);
+};
 
 display_name("Аноним");
 
-
-function send_message(){
-	socket.emit("new message", {message: message.value})
+function sending_m() {
+  socket.emit("new_message", { message: message.value });
+//   console.log("несл")
 }
 
-send_message.onclick = () =>{
-	send_message()
-}
+send_message.onclick = () => {
+  sending_m();
+};
 
-document.addEventListener("keypress", (e)=>{
-	if(e.keyCode == 13){
-		send_message()
-	}
-})
+document.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    sending_m();
+  }
+});
 
+socket.on("add_mes", (data) => {
+  let { username, message } = data;
 
-socket.on("add_mes", (data)=>{
-	let {username, message} = data;
-
-	feedback.insertAdjacentHTML("beforeend", 
-		`<h5 class="has-text-light mes-single has-background-dark">
+  feedback.insertAdjacentHTML(
+    "beforeend",
+    `<h5 class="has-text-light mes-single has-background-dark">
              <span>${username}</span>
-                ${message} </h5>`);
-	clear(message)
-})
-function clear(input){
-	input.value = '';
+                ${message} </h5>`
+  );
+  clear(message);
+});
+function clear(input) {
+  input.value = "";
 }
-
-
-
-
